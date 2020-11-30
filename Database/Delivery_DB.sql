bc848db3ad667de1b2540d0063df358d40fa4c72
Alejandro <alejandroc0701@gmail.com>
2020-11-30 18:43:54 -0400

    Revert "Update Delivery_DB.sql"
    This reverts commit d3a9d7ae30cbaf0b9f2ba6f5d8ae4533e3d7e09a.

--- Database/Delivery_DB.sql
+++ Database/Delivery_DB.sql
@@ -8,6 +8,7 @@ CREATE OR REPLACE TYPE datos_basicos AS OBJECT
 (
  nombre_usuario VARCHAR(25),
  correo_electronico VARCHAR(50),
+ foto BLOB,
  STATIC FUNCTION Validar_Nombre(nombre VARCHAR) RETURN VARCHAR,
  STATIC FUNCTION Validar_Correo(correo VARCHAR) RETURN VARCHAR
 );
@@ -112,7 +113,6 @@ END;
 CREATE TABLE aliado_comercial(
 id_aliado NUMBER NOT NULL,
 sector CHAR NOT NULL,
-foto BLOB,
 datos datos_basicos,
 CONSTRAINT PK_prod PRIMARY KEY(id_aliado),
 CONSTRAINT check_sector CHECK(sector in ('A','F','R','T'))
@@ -121,7 +121,6 @@ CONSTRAINT check_sector CHECK(sector in ('A','F','R','T'))
 
 CREATE TABLE proveedor(
 id_proveedor NUMBER NOT NULL,
-foto BLOB,
 datos_prov datos_basicos,
 CONSTRAINT PK_prov PRIMARY KEY(id_proveedor)
 );
@@ -134,7 +133,6 @@ primer_apellido VARCHAR(20) NOT NULL,
 segundo_nombre VARCHAR(20) ,
 segundo_apellido VARCHAR(20) ,
 datos datos_basicos,
-foto BLOB,
 CONSTRAINT PK_usuario PRIMARY KEY(id_usuario)
 );
 
