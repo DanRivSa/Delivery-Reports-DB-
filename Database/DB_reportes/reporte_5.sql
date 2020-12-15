22f9e262e14b0415d4eea6f288174ea7a2156920
andrea_m <andreamendozag2@gmail.com>
2020-12-15 03:21:23 -0400

    reporte 5

--- Database/DB_reportes/reporte_5.sql
+++ Database/DB_reportes/reporte_5.sql
@@ -3,31 +3,39 @@ A.FOTO AS logo_ali,P.FOTO AS logo_prov M.Nombre
 as Municipio, Count (*) as Envios
 From Aliado_Comercial A, Sucursal S, Sucursal_Asignada SA, Estado E, Municipio M, Proveedor P
 Where (sa.id_suc = S.id_sucursal) and (S.id_aliado = A.id_aliado) and (sa.est_suc = e.codigo_estado) 
-and (sa.mun_suc = m.codigo_municipio) and (sa.id_prov = p.id_proveedor) AND ( E. Nombre = 'Distrito Capital')
+and (sa.mun_suc = m.codigo_municipio) and (sa.id_prov = p.id_proveedor) AND ( E. Nombre = 'Miranda')
 Group by E.Nombre, M.Nombre, A.Nombre, P.DATOS_PROV.NOMBRE_USUARIO
 order by Envios desc;
 
 
 
-CREATE OR REPLACE PROCEDURE reporte_5(cur OUT SYS_REFCURSOR,estado VARCHAR2,cod_estado INT) IS
+
+
+CREATE OR REPLACE PROCEDURE reporte_5(cur OUT SYS_REFCURSOR,estado VARCHAR2) IS
 BEGIN
     OPEN cur FOR
-        Select 
+Select 
         E. Nombre as Estado, 
         A.Nombre as Empresa, 
+        A.Foto AS logo_ali,
         P.DATOS_PROV.NOMBRE_USUARIO as Proveedor,
+        P.Foto AS logo_prov,
         m.Nombre as Municipio,
         d.id_direccion,
-       (SELECT COUNT(*) FROM envio v WHERE v.id_dir = d.id_direccion and d.cod_es = cod_estado and m.codigo_municipio = d.cod_mun ) AS ENVIOS,
-        A.Foto AS logo_ali,
-        P.Foto AS logo_prov
-FROM ENVIO v
-
-        INNER JOIN aliado_Comercial A ON v.id_aliado = A.id_aliado
-        INNER JOIN direccion d on v.id_dir = d.id_direccion
+       (SELECT COUNT(*) FROM envio en WHERE 
+                                            en.id_prov = p.id_proveedor and
+                                            en.id_aliado = A.id_aliado  and
+                                            en.id_dir = d.id_direccion
+                                            ) AS ENVIOS
+        
+FROM envio en
+        INNER JOIN direccion d on en.id_dir = d.id_direccion
         INNER JOIN estado e on d.cod_es = e.codigo_estado
         INNER JOIN municipio m on d.cod_mun = m.codigo_municipio
-        INNER JOIN proveedor p ON v.id_prov = p.id_proveedor
+        INNER JOIN acuerdo_servicio ac on en.id_acuerdo = ac.id_acuerdo
+        INNER JOIN aliado_Comercial A ON ac.id_aliado = A.id_aliado
+        INNER JOIN proveedor p ON ac.id_prov_serv = p.id_proveedor
+        INNER JOIN sede s ON en.id_sede_u = s.id_sede
         
 WHERE E. Nombre = estado AND rownum < 5
 order by Envios desc;
