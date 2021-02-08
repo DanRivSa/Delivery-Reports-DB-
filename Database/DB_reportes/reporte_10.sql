7c22cb0d73bd26e856915b162d31e87acc8f3d3e
Alejandro <alejandroc0701@gmail.com>
2021-02-08 18:21:46 -0400

    Update reporte_10.sql
    Funcionando reporte 10, solo falta probar si sirve la llamada del procedimiento

--- Database/DB_reportes/reporte_10.sql
+++ Database/DB_reportes/reporte_10.sql
@@ -1,11 +1,10 @@
 --REPORTE 10
 --SELECT DE PRUEBA 
-Select ase.fechas.fecha_inicio as Fecha_Inicio, ase.fechas.fecha_fin as Fecha_Fin, ac.nombre as Comercio, P.DATOS_PROV.NOMBRE_USUARIO as Proveedor_Servicio, P.Foto as Logo_Proveedor,
-    (select '$' || sum(ase.precio_as)
-    from acuerdo_Servicio ase, proveedor p
-    where (ase.id_prov_serv = p.id_proveedor) and (P.DATOS_PROV.NOMBRE_USUARIO = 'Traetelo')) as Ingresos
+Select ase.fechas.fecha_inicio as Fecha_Inicio, ase.fechas.fecha_fin as Fecha_Fin, ac.nombre as Comercio, P.DATOS_PROV.NOMBRE_USUARIO as Proveedor_Servicio, P.Foto as Logo_Proveedor, (select '$' || sum(ase.precio_as)
+                                                                                                                                                                                        from acuerdo_Servicio ase, proveedor p
+                                                                                                                                                                                        where (ase.id_prov_serv = p.id_proveedor) and (P.DATOS_PROV.NOMBRE_USUARIO = 'Ego')) as Ingresos
 from aliado_comercial ac, proveedor p, acuerdo_servicio ase
-where (ase.id_prov_serv = p.id_proveedor) and (ase.id_aliado = ac.id_aliado) and (P.DATOS_PROV.NOMBRE_USUARIO = 'Traetelo') and (ase.fechas.fecha_inicio >= '10/01/20') and (ase.fechas.fecha_inicio <= '10/01/22') ;
+where (ase.id_prov_serv = p.id_proveedor) and (ase.id_aliado = ac.id_aliado) and (P.DATOS_PROV.NOMBRE_USUARIO = 'Ego') and (ase.fechas.fecha_inicio >= to_date('10-01-2020','DD-MM-YYYY')) and (ase.fechas.fecha_inicio <= to_date('10-01-2021','DD-MM-YYYY'));
 
 --INICIO DEL REPORTE PROCEDURE
 
@@ -16,25 +15,22 @@ BEGIN
     from aliado_comercial ac, proveedor p, acuerdo_servicio ase
     where (ase.id_prov_serv = p.id_proveedor) and (ase.id_aliado = ac.id_aliado) and (P.DATOS_PROV.NOMBRE_USUARIO = parametro_nombreproveedor) and (ase.fechas.fecha_inicio >= parametro_finicio) and (ase.fechas.fecha_inicio <= parametro_ffinal) ;
 
-IF cant_registros > 0 AND parametro_nombreproveedor != null AND parametro_finicio !=null AND parametro_ffinal !=null THEN
+IF cant_registros > 0 THEN
     OPEN cur10 FOR
-    Select ase.fechas.fecha_inicio as Fecha_Inicio, ase.fechas.fecha_fin as Fecha_Fin, ac.nombre as Comercio, P.DATOS_PROV.NOMBRE_USUARIO as Proveedor_Servicio, P.Foto as Logo_Proveedor,
-    (select '$' || sum(ase.precio_as)
-    from acuerdo_Servicio ase, proveedor p
-    where (ase.id_prov_serv = p.id_proveedor) and (P.DATOS_PROV.NOMBRE_USUARIO = parametro_nombreproveedor)) as Ingresos
-from aliado_comercial ac, proveedor p, acuerdo_servicio ase
-where (ase.id_prov_serv = p.id_proveedor) and (ase.id_aliado = ac.id_aliado) and (P.DATOS_PROV.NOMBRE_USUARIO = parametro_nombreproveedor) and (ase.fechas.fecha_inicio >= parametro_finicio) and (ase.fechas.fecha_inicio <= parametro_ffinal) ;
+        Select ase.fechas.fecha_inicio as Fecha_Inicio, ase.fechas.fecha_fin as Fecha_Fin, ac.nombre as Comercio, P.DATOS_PROV.NOMBRE_USUARIO as Proveedor_Servicio, P.Foto as Logo_Proveedor, (select '$' || sum(ase.precio_as)
+                                                                                                                                                                                                from acuerdo_Servicio ase, proveedor p
+                                                                                                                                                                                                where (ase.id_prov_serv = p.id_proveedor) and (P.DATOS_PROV.NOMBRE_USUARIO = parametro_nombreproveedor)) as Ingresos
+        from aliado_comercial ac, proveedor p, acuerdo_servicio ase
+        where (ase.id_prov_serv = p.id_proveedor) and (ase.id_aliado = ac.id_aliado) and (P.DATOS_PROV.NOMBRE_USUARIO = parametro_nombreproveedor) and (ase.fechas.fecha_inicio >= parametro_finicio) and (ase.fechas.fecha_inicio <= parametro_ffinal) ;
 
-    ELSE
+ELSE
     OPEN cur10 FOR
-    Select ase.fechas.fecha_inicio as Fecha_Inicio, ase.fechas.fecha_fin as Fecha_Fin, ac.nombre as Comercio, P.DATOS_PROV.NOMBRE_USUARIO as Proveedor_Servicio, P.Foto as Logo_Proveedor,
-        (select '$' || sum(ase.precio_as)
-        from acuerdo_Servicio ase, proveedor p
-        where (ase.id_prov_serv = p.id_proveedor) ) as Ingresos
-    from aliado_comercial ac, proveedor p, acuerdo_servicio ase
-    where (ase.id_prov_serv = p.id_proveedor) and (ase.id_aliado = ac.id_aliado);
-    END IF;
+        Select ase.fechas.fecha_inicio as Fecha_Inicio, ase.fechas.fecha_fin as Fecha_Fin, ac.nombre as Comercio, P.DATOS_PROV.NOMBRE_USUARIO as Proveedor_Servicio, P.Foto as Logo_Proveedor, (select '$' || sum(ase.precio_as)
+                                                                                                                                                                                                from acuerdo_Servicio ase, proveedor p
+                                                                                                                                                                                                where (ase.id_prov_serv = p.id_proveedor) ) as Ingresos
+        from aliado_comercial ac, proveedor p, acuerdo_servicio ase
+        where (ase.id_prov_serv = p.id_proveedor) and (ase.id_aliado = ac.id_aliado);
+END IF;
 END;
 
---LLAMADA JASPERSOFT
 {CALL reporte10($P{ORACLE_REF_CURSOR},$P{parametro_nombreproveedor},$P{parametro_finicio}, $P{parametro_ffinal} )}
